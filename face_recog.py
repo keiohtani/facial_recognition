ee01af4ca6a164fd8f45c7a5016120bc3a19358d
Kei <35276788+keiohtani@users.noreply.github.com>
2019-03-03 22:10:19 -0500

    add function to load known faces and save unknown faces with a new id

--- face_recog.py
+++ face_recog.py
@@ -1,18 +1,81 @@
 from PIL import Image, ImageDraw
 import face_recognition
-import os
-import cv2
-image_path = os.getcwd() + '/sample.jpg'
-image = face_recognition.load_image_file(image_path)
-face_locations = face_recognition.face_locations(image)
+ID_FILE = 'ids.txt'
 
-pil_image = Image.fromarray(image)
+def draw_boxes(pil_image, unknown_face_locations, unknown_face_encodings, known_face_encodings, known_face_names):
+    draw = ImageDraw.Draw(pil_image)
+    for (top, right, bottom, left), unknown_face_encoding in zip(unknown_face_locations, unknown_face_encodings):
 
-draw = ImageDraw.Draw(pil_image)
+        matches = face_recognition.compare_faces(known_face_encodings, unknown_face_encoding)
 
-for (top, right, bottom, left) in face_locations:
-    draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+        name = "unknown"
 
-del draw
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = str(known_face_names[first_match_index])
+        
+        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+        # Draw a label with a name below the face
+        text_width, text_height = draw.textsize(name)
+        draw.rectangle(((left, bottom + text_height + 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+        draw.text((left + 6, bottom + text_height - 3), name, fill=(255, 255, 255, 255))
+    del draw
+    pil_image.show()
+    return pil_image
 
-pil_image.show()
+def load_known_faces():
+    known_face_encodings = []
+    known_face_names = []
+    with open(ID_FILE, mode='r') as f:
+        last_id = int(f.read())
+    for i in range(1, last_id):
+        image_path = 'known_faces/' + str(i) + '.jpg'
+        known_image = face_recognition.load_image_file(image_path)
+        encoding = face_recognition.face_encodings(known_image)[0]
+        known_face_encodings.append(encoding)
+        known_face_names.append(i)
+    print('face data is loaded from', ID_FILE)
+    return known_face_encodings, known_face_names
+    
+    
+def compare_faces(known_face_encodings, known_face_names):
+    unknown_image = face_recognition.load_image_file('sample.jpg')
+
+    unknown_face_locations = face_recognition.face_locations(unknown_image)
+    unknown_face_encodings = face_recognition.face_encodings(unknown_image)
+
+    results = face_recognition.compare_faces(known_face_encodings, unknown_face_encodings[0])
+    for (top, right, bottom, left), unknown_face_encoding in zip(unknown_face_locations, unknown_face_encodings):
+
+        matches = face_recognition.compare_faces(known_face_encodings, unknown_face_encoding)
+
+        name = "unknown"
+
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
+            print(name)
+    
+def save_face_image(pil_image, face_locations):
+    margin = 15
+    # read id file to keep track of the last id for students
+    with open(ID_FILE, mode='r') as f:
+        person_id = int(f.readline()) + 1
+    person_id = 1
+    for (top, right, bottom, left) in face_locations:
+        cropped_image = pil_image.crop((left - margin, top - margin, right + margin, bottom + margin))
+        cropped_image.save('known_faces/' + str(person_id) + '.jpg')
+        person_id = person_id + 1
+    with open(ID_FILE, mode='w') as f:
+        f.write(str(person_id))
+
+if __name__ == '__main__':
+    image_path = 'sample.jpg'
+    image = face_recognition.load_image_file(image_path)
+    face_locations = face_recognition.face_locations(image)
+    face_encodings = face_recognition.face_encodings(image, face_locations)
+    pil_image = Image.fromarray(image)
+    known_face_encodings, known_face_names = load_known_faces()
+    save_face_image(pil_image, face_locations)
+    draw_boxes(pil_image, face_locations, face_encodings, known_face_encodings, known_face_names)
+    # compare_faces(known_face_encodings, known_face_names)
