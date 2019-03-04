12dd2869834a4e4871f9b8d64b443a49a80cf8f5
Kei <35276788+keiohtani@users.noreply.github.com>
2019-03-04 12:15:58 -0500

    implement sort for save_unknown_faces

--- face_recog.py
+++ face_recog.py
@@ -1,6 +1,16 @@
 from PIL import Image, ImageDraw
 import face_recognition
+import os
+
+
+#TODO
+# - crop pictures 
+# - add them into google photos
+
 ID_FILE = 'ids.txt'
+UNKNOWN_FACE_DIR = 'unknown_faces'
+KNOWN_FACE_DIR = 'known_faces'
+CROP_MARGIN = 15
 
 def draw_boxes(pil_image, unknown_face_locations, unknown_face_encodings, known_face_encodings, known_face_names):
     draw = ImageDraw.Draw(pil_image)
@@ -23,18 +33,20 @@ def draw_boxes(pil_image, unknown_face_locations, unknown_face_encodings, known_
     pil_image.show()
     return pil_image
 
-def load_known_faces():
+def load_known_faces(known_face_dir):
     known_face_encodings = []
     known_face_names = []
     with open(ID_FILE, mode='r') as f:
         last_id = int(f.read())
-    for i in range(1, last_id):
-        image_path = 'known_faces/' + str(i) + '.jpg'
+
+    dir_list = os.listdir(known_face_dir)
+    for i in dir_list:
+        image_path = 'known_faces/' + str(i) + '/test.jpg'
         known_image = face_recognition.load_image_file(image_path)
         encoding = face_recognition.face_encodings(known_image)[0]
         known_face_encodings.append(encoding)
         known_face_names.append(i)
-    print('face data is loaded from', ID_FILE)
+    print('face data is loaded')
     return known_face_encodings, known_face_names
     
     
@@ -57,15 +69,13 @@ def compare_faces(known_face_encodings, known_face_names):
             print(name)
     
 def save_face_image(face_locations):
-    margin = 15
-
     known_face_encodings, known_face_names = load_known_faces()
     # read id file to keep track of the last id for students
     with open(ID_FILE, mode='r') as f:
         person_id = int(f.readline())
     
     for (top, right, bottom, left) in face_locations:
-        cropped_image = pil_image.crop((left - margin, top - margin, right + margin, bottom + margin))
+        cropped_image = pil_image.crop((left - CROP_MARGIN, top - CROP_MARGIN, right + CROP_MARGIN, bottom + CROP_MARGIN))
         cropped_image.save('known_faces/' + str(person_id) + '.jpg')
         person_id = person_id + 1
 
@@ -73,7 +83,7 @@ def save_face_image(face_locations):
         f.write(str(person_id))
 
 def save_unknown_faces(unknown_face_locations, unknown_face_encodings):
-    margin = 15
+    
     
     known_face_encodings, known_face_names = load_known_faces()
 
@@ -84,9 +94,12 @@ def save_unknown_faces(unknown_face_locations, unknown_face_encodings):
     for (top, right, bottom, left), unknown_face_encoding in zip(unknown_face_locations, unknown_face_encodings):
 
         matches = face_recognition.compare_faces(known_face_encodings, unknown_face_encoding)
+        print(matches)
         print("face found")
-        if not all(matches):
-            cropped_image = pil_image.crop((left - margin, top - margin, right + margin, bottom + margin))
+        if any(matches):
+            None
+        else:
+            cropped_image = pil_image.crop((left - CROP_MARGIN, top - CROP_MARGIN, right + CROP_MARGIN, bottom + CROP_MARGIN))
             new_image_path = 'known_faces/' + str(person_id) + '.jpg'
             cropped_image.save(new_image_path)
             print('face data is saved to', str(new_image_path))
@@ -97,18 +110,57 @@ def save_unknown_faces(unknown_face_locations, unknown_face_encodings):
     with open(ID_FILE, mode='w') as f:
         f.write(str(person_id))
 
-if __name__ == '__main__':
+def save_unknown_faces_into_direcotry(unknown_face_dir, known_face_dir):
+    dir_list = os.listdir(unknown_face_dir)
 
+    # read id file to keep track of the last id for students
+    with open(ID_FILE, mode='r') as f:
+        person_id = int(f.readline())
+
+    for image_path in dir_list:
+        known_face_encodings, known_face_names = load_known_faces(known_face_dir)
+        image = face_recognition.load_image_file(unknown_face_dir + '/' + image_path)
+        unknown_face_locations = face_recognition.face_locations(image)
+        unknown_face_encodings = face_recognition.face_encodings(image, unknown_face_locations)
+        pil_image = Image.fromarray(image)
+
+        if (not known_face_encodings):
+            for (top, right, bottom, left), unknown_face_encoding in zip(unknown_face_locations, unknown_face_encodings):
+                cropped_image = pil_image.crop((left - CROP_MARGIN, top - CROP_MARGIN, right + CROP_MARGIN, bottom + CROP_MARGIN))
+                new_image_path = 'known_faces/' + str(person_id) + '/test.jpg'
+                print('a new face is found')
+                if (not os.path.isdir('known_faces/' + str(person_id))):
+                    os.mkdir('known_faces/' + str(person_id))
+                cropped_image.save(new_image_path)
+                print('face data is saved to', str(new_image_path))
+                person_id = person_id + 1
+        else: 
+            for (top, right, bottom, left), unknown_face_encoding in zip(unknown_face_locations, unknown_face_encodings):
+                matches = face_recognition.compare_faces(known_face_encodings, unknown_face_encoding)
+                print(matches)
+                if any(matches):
+                    None
+                else:
+                    cropped_image = pil_image.crop((left - CROP_MARGIN, top - CROP_MARGIN, right + CROP_MARGIN, bottom + CROP_MARGIN))
+                    new_image_path = 'known_faces/' + str(person_id) + '/test.jpg'
+                    print('a new face is found')
+                    if (not os.path.isdir('known_faces/' + str(person_id))):
+                        os.mkdir('known_faces/' + str(person_id))
+                        print("make a new dir")
+                    cropped_image.save(new_image_path)
+                    print('face data is saved to', str(new_image_path))
+                    person_id = person_id + 1
+
+    print("all the face data in the photo is saved. ")
+    with open(ID_FILE, mode='w') as f:
+        f.write(str(person_id))
+
+if __name__ == '__main__':
     # loading image
-    image_path = 'sample_4.jpg'
-    image = face_recognition.load_image_file(image_path)
-    face_locations = face_recognition.face_locations(image)
-    face_encodings = face_recognition.face_encodings(image, face_locations)
-    pil_image = Image.fromarray(image)
-    pil_image.show()
+    save_unknown_faces_into_direcotry(UNKNOWN_FACE_DIR, KNOWN_FACE_DIR)
 
     # known_face_encodings, known_face_names = load_known_faces()
-    # save_face_image(face_locations)
-    # draw_boxes(pil_image, face_locations, face_encodings, known_face_encodings, known_face_names)
+    # save_face_image(unknown_face_locations)
+    # draw_boxes(pil_image, unknown_face_locations, unknown_face_encodings, known_face_encodings, known_face_names)
     # compare_faces(known_face_encodings, known_face_names)
-    save_unknown_faces(face_locations, face_encodings)
+    # save_unknown_faces(unknown_face_locations, unknown_face_encodings)
