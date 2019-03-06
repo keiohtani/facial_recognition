f8eff838449635d5d2fbc8de119ce4e425789a4a
Kei <35276788+keiohtani@users.noreply.github.com>
2019-03-06 14:19:58 -0500

    add an id tracker

--- downloader.py
+++ downloader.py
@@ -1,5 +1,4 @@
 import authentication
-import requests
 import json
 import urllib.request
 import face_recog
@@ -8,16 +7,17 @@ import face_recog
 UNKNOWN_FACE_PATH = 'unknown_faces'
 
 def download_people_images():
+
     service = authentication.get_authenticated_service()
-    url = 'https://photoslibrary.googleapis.com/v1/mediaItems:search'
-    headers = {
-        'Authorization': "Bearer " + service._http.request.credentials.access_token,
-    }
+
     with open('downloader_payload.json') as f:
         payload = json.loads(f.read())
     
     nextPageToken = ''
-    photo_id = 1
+    with open('id.txt') as f:
+        photo_id = int(f.readline())
+        print('Photo id', photo_id, 'is loaded.')
+    
     while True:
         media_list = service.mediaItems().search(body=payload).execute()
         if 'mediaItems' not in media_list:  # when no items are found
@@ -25,13 +25,17 @@ def download_people_images():
         for mediaItem in media_list['mediaItems']:
             image_url = mediaItem['baseUrl'] # the size can be set by adding '=w2048-h1024' at the end of URL
             urllib.request.urlretrieve(image_url + '=w1024', 'temp.jpg')
-            face_recog.save_face_from_path('temp.jpg', photo_id)
-            photo_id = photo_id + 1
+            photo_id = face_recog.save_face_from_path('temp.jpg', photo_id)
         if 'nextPageToken' not in media_list:
             break
         print('next page')
         nextPageToken = media_list['nextPageToken']
         payload['pageToken'] = nextPageToken
+
+    with open('id.txt') as f:
+        f.write(photo_id)
+        print('Photo id', photo_id, 'is saved.')
+
     print('finished downloading pictures')
 
 if __name__ == "__main__":
