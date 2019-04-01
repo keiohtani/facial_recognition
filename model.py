36bb40c66cc0987a299b76430846abdcc1625892
Kei <35276788+keiohtani@users.noreply.github.com>
2019-03-31 21:42:08 -0400

    improve the accuracy by changing the model and adjusting the input size

--- model.py
+++ model.py
@@ -1,22 +1,22 @@
 # https://sefiks.com/2018/09/03/face-recognition-with-facenet-in-keras/
 
 
-from keras.applications.vgg16 import VGG16
+from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
 import numpy as np
 from keras.preprocessing.image import load_img, save_img, img_to_array
 from keras.applications.imagenet_utils import preprocess_input
 
 def preprocess_image(image_path):
-    img = load_img(image_path, target_size=(160, 160))
+    img = load_img(image_path, target_size=(INPUT_SIZE, INPUT_SIZE))
     img = img_to_array(img)
     img = np.expand_dims(img, axis=0)
     img = preprocess_input(img)
     return img
 
-INPUT_SIZE = 96
+INPUT_SIZE = 50
 
-model = VGG16(weights='imagenet', include_top=False)
+model = ResNet50(weights='imagenet', include_top=False)
 
 # from keras.layers import Dense, Flatten
 # from keras.models import Sequential
@@ -36,7 +36,7 @@ def findEuclideanDistance(source_representation, test_representation):
     euclidean_distance = np.sqrt(euclidean_distance)
     return euclidean_distance
 
-img1_representation = l2_normalize(model.predict(preprocess_image('img4.jpg'))[0,:])
+img1_representation = l2_normalize(model.predict(preprocess_image('img1.jpg'))[0,:])
 img2_representation = l2_normalize(model.predict(preprocess_image('img5.jpg'))[0,:])
 
 euclidean_distance = findEuclideanDistance(img1_representation, img2_representation)
