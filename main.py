dd85539183a6e932710d3b0d6553dc1a7bf6b4b1
Kei <35276788+keiohtani@users.noreply.github.com>
2019-04-08 12:32:31 -0400

    add vectorized implementation of euclidean distance and cosine distance

--- main.py
+++ main.py
@@ -4,91 +4,105 @@ from preprocess import *
 from model import *
 import cv2
 import os
-
-def findEuclideanDistance(source_representation, test_representation):
-    euclidean_distance = source_representation - test_representation
-    euclidean_distance = np.sum(np.multiply(euclidean_distance, euclidean_distance))
-    euclidean_distance = np.sqrt(euclidean_distance)
-    return euclidean_distance
-
-def findCosineDistance(source_representation, test_representation):
-    a = np.matmul(np.transpose(source_representation), test_representation)
-    b = np.sum(np.multiply(source_representation, source_representation))
-    c = np.sum(np.multiply(test_representation, test_representation))
-    return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
+import numpy as np
 
 
-def verifyFace(model, img1, img2):
-    epsilon = 0.40
-
-    img1_representation = model.predict(preprocess_image(img1))[0,:]
-    img2_representation = model.predict(preprocess_image(img2))[0,:]
+class Facial_Recogition():
     
-    # euclidean_distance = findEuclideanDistance(img1_representation, img2_representation)
-    cosine_similarity = findCosineDistance(img1_representation, img2_representation)
-    print(cosine_similarity)
-    if(cosine_similarity < epsilon):
-        print("verified... they are same person")
-    else:
-        print("unverified! they are not same person!")
+    def __init__(self):
+        self.model = VGG_face_model()
+        # remove the last two layers to get 128 dimensions vector 
+        self.model = Model(inputs=self.model.layers[0].input, outputs=self.model.layers[-2].output)
+        self.dir_path = 'face_database'
+        self.image_dir_list = os.listdir(self.dir_path)
+        if ('.DS_Store' in self.image_dir_list):
+            self.image_dir_list.remove('.DS_Store')
+        print('.DS_Store is removed.')
 
-def calculate_distance(model, img1, img2):
-    epsilon = 0.40
 
-    img1_representation = model.predict(preprocess_image(img1))[0,:]
-    img2_representation = model.predict(preprocess_image(img2))[0,:]
-    
-    # euclidean_distance = findEuclideanDistance(img1_representation, img2_representation)
-    cosine_similarity = findCosineDistance(img1_representation, img2_representation)
-    return cosine_similarity
 
-def realtime_verification():
-    INTERVAL = 30
-    DEVICE_ID = 0  
-    ESC_KEY = 27
-    FRAME_RATE = 24
-    WINDOW_NAME = 'facial recognition'
+    def calculate_distance(self, img1, img2):
 
-    cap = cv2.VideoCapture(DEVICE_ID)
-    end_flag, c_frame = cap.read()
-    # print('Press esc to exit.')
+        epsilon = 0.40
 
-    # while end_flag == True:
+        img1_representation = self.model.predict(preprocess_image(img1))[0,:]
+        img2_representation = self.model.predict(preprocess_image(img2))[0,:]
+        
+        # euclidean_distance = findEuclideanDistance(img1_representation, img2_representation)
+        cosine_similarity = self.findCosineDistance(img1_representation, img2_representation)
+        return cosine_similarity
 
-    #     cv2.imshow(WINDOW_NAME, c_frame)
 
-    #     key = cv2.waitKey(INTERVAL)
-    #     if key == ESC_KEY:
-    #         break
+    def realtime_verification(self):
+        INTERVAL = 30
+        DEVICE_ID = 0  
+        ESC_KEY = 27
+        FRAME_RATE = 24
+        WINDOW_NAME = 'facial recognition'
 
-    #     end_flag, c_frame = cap.read()
-    
-    #exitting
-    cv2.destroyAllWindows()
-    cap.release()
+        cap = cv2.VideoCapture(DEVICE_ID)
+        end_flag, c_frame = cap.read()
+        # print('Press esc to exit.')
 
+        # while end_flag == True:
+
+        #     cv2.imshow(WINDOW_NAME, c_frame)
+
+        #     key = cv2.waitKey(INTERVAL)
+        #     if key == ESC_KEY:
+        #         break
+
+        #     end_flag, c_frame = cap.read()
+        
+        #exitting
+        cv2.destroyAllWindows()
+        cap.release()
+
+
+    def recognize_vector_euclidean_distance(self, test_image):
+
+        epsilon = 120
+        
+        test_representation = self.model.predict(preprocess_image(test_image))[0,:]
+        dataset = []
+
+        for stored_image in self.image_dir_list:
+            img_representation = self.model.predict(preprocess_image('face_database/' + stored_image))[0,:]
+            dataset.append(img_representation)
+
+        dataset = np.array(dataset)
+        euclidean_distance = dataset - test_representation
+        euclidean_distance = np.sum(euclidean_distance * euclidean_distance, axis=1)
+        euclidean_distance = np.sqrt(euclidean_distance)
+
+        if euclidean_distance.min() < epsilon:
+            index = np.argmin(euclidean_distance)
+            print(self.image_dir_list[index])
+
+
+    def recognize_vector_cosine_distance(self, test_image):
+
+        epsilon = 0.4
+        
+        test_representation = self.model.predict(preprocess_image(test_image))[0,:]
+        dataset = []
+
+        for stored_image in self.image_dir_list:
+            img_representation = self.model.predict(preprocess_image('face_database/' + stored_image))[0,:]
+            dataset.append(img_representation)
+
+        dataset = np.array(dataset)
+        a = np.matmul(dataset, test_representation)
+        b = np.sum(dataset * dataset, axis = 1)
+        c = np.sum(np.multiply(test_representation, test_representation))
+        x = 1 - (a / (np.sqrt(b) * np.sqrt(c)))
+        if x.min() < epsilon:
+            index = np.argmin(x)
+            print(self.image_dir_list[index])
 
 
 if __name__ == '__main__':
     test_image = 'test.jpg'
-    model = VGG_face_model()
-    face_model = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
-
-    dir_path = 'face_database'
-    image_dir_list = os.listdir(dir_path)
-
-    if ('.DS_Store' in image_dir_list):
-        image_dir_list.remove('.DS_Store')
-        print('.DS_Store is removed.')
-
-    min_distance = 1.0
-
-    for stored_image in image_dir_list:
-        distance = calculate_distance(face_model, test_image, dir_path + '/' + stored_image)
-        print(distance)
-        if min_distance > distance:
-            min_distance = distance
-            name = stored_image
-    
-    if (min_distance < 0.4):
-        print(name)
+    fr = Facial_Recogition()
+    # fr.recognize()
+    fr.recognize_vector_cosine_distance(test_image)
