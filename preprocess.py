f1d82861dd08de7825893c9548efb97453102066
Kei <35276788+keiohtani@users.noreply.github.com>
2019-04-08 15:52:18 -0400

    add a new preprocess func for captured images

--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,18 @@
 from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.vgg16 import preprocess_input
+from cv2 import resize
 import numpy as np
 
-def preprocess_image(image_path):
+def preprocess_image_from_path(image_path):
     img = load_img(image_path, target_size=(224, 224))
     img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    return img
+
+
+def preprocess_opencv_image(image):
+    img = resize(image, (224, 224))
     img = np.expand_dims(img, axis=0)
     img = preprocess_input(img)
     return img
