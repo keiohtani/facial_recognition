2b124481afa55b555059bb19ab5ecdbbacc36106
Kei <35276788+keiohtani@users.noreply.github.com>
2019-04-09 20:20:41 -0400

    combine preprocess into Facial_Recognition class in main.py and clean the functions

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 # https://sefiks.com/2018/08/06/deep-face-recognition-with-keras/
 # https://medium.com/@sumantrajoshi/face-recognizer-application-using-a-deep-learning-model-python-and-keras-2873e9aa6ab3
-
-from preprocess import *
 from model import *
 import cv2
 import os
 import numpy as np
 from align import AlignDlib
-
+from keras.preprocessing.image import load_img, img_to_array
+from keras.applications.vgg16 import preprocess_input
+from cv2 import resize
 
 
 class Facial_Recogition():
 
-    def __init__(self, model='VGG16'):
-        if model == 'Inception':
-            self.model, self.input_size = Inception_Model()
-        else: 
-            self.model, self.input_size = VGG_face_model()
+    def __init__(self, architecture='VGG16'):
+        self.architecture = architecture
+        if architecture == 'Inception':
+            self.model = Inception_Model()
+            self.input_size = 96
+        elif architecture == 'VGG16':
+            self.model = VGG_face_model()
+            self.input_size = 224
+        else:
+            print('A valid name for model needs to be passed.')
         self.dir_path = 'face_database'
         self.image_dir_list = os.listdir(self.dir_path)
 
@@ -25,69 +30,56 @@ class Facial_Recogition():
             print('.DS_Store is removed.')
 
         self.np_dataset = self.load_database()
-        self.face_haarcascade = cv2.CascadeClassifier(
-            'haarcascade_frontalface_default.xml')
         print('Database is loaded.')
         self.alignment = AlignDlib('models/landmarks.dat')
 
     def load_database(self):
 
         dataset = []
-
         for stored_image in self.image_dir_list:
             img_representation = self.model.predict(
-                preprocess_image_from_path('face_database/' + stored_image, self.input_size))[0, :]
+                self.preprocess('face_database/' + stored_image))[0, :]
             dataset.append(img_representation)
 
         return np.array(dataset)
 
-    def find_euclidean_distance(self, test_representation):
+    def find_distance(self, test_representation, alg='cosine'):
+        if alg == 'cosine':
+            epsilon = 0.3
+            a = np.matmul(self.np_dataset, test_representation)
+            b = np.sum(self.np_dataset * self.np_dataset, axis=1)
+            c = np.sum(np.multiply(test_representation, test_representation))
+            distance = 1 - (a / (np.sqrt(b) * np.sqrt(c)))
 
-        epsilon = 120
-        euclidean_distance = self.np_dataset - test_representation
-        euclidean_distance = np.sum(
-            euclidean_distance * euclidean_distance, axis=1)
-        euclidean_distance = np.sqrt(euclidean_distance)
+        elif alg == 'euclidean':
+            epsilon = 100
+            euclidean_distance = self.np_dataset - test_representation
+            euclidean_distance = np.sum(
+                euclidean_distance * euclidean_distance, axis=1)
+            distance = np.sqrt(euclidean_distance)
+        
+        else:
+            print('Pass either cosine or euclidean for alg')
 
-        if euclidean_distance.min() < epsilon:
-            index = np.argmin(euclidean_distance)
+        print(distance)
+        print(self.image_dir_list)
+
+        if distance.min() < epsilon:
+            index = np.argmin(distance)
             name = self.image_dir_list[index]
             return name
 
-    def find_cosine_distance(self, test_representation):
-
-        epsilon = 0.4
-        a = np.matmul(self.np_dataset, test_representation)
-        b = np.sum(self.np_dataset * self.np_dataset, axis=1)
-        c = np.sum(np.multiply(test_representation, test_representation))
-        x = 1 - (a / (np.sqrt(b) * np.sqrt(c)))
-
-        if x.min() < epsilon:
-            index = np.argmin(x)
-            name = self.image_dir_list[index]
-            return name
-
-    def recognize_from_path(self, image_path):
+    def recognize(self, image_path):
         test_representation = self.model.predict(
-            preprocess_image_from_path(image_path, self.input_size))[0, :]
-        self.find_cosine_distance(test_representation)
+            self.preprocess(image_path))[0, :]
+        self.find_distance(test_representation)
 
-    def recognize_capture(self):
-        c_frame = self.capture()
-        img, c_frame, faces = self.haarcascade_crop_face(c_frame)
-        if img != []:
-            test_representation = self.model.predict(
-                preprocess_opencv_image(img, self.input_size))[0, :]
-            self.find_euclidean_distance(test_representation)
-
-    def recognize_realtime(self):
+    def recognize(self):
 
         INTERVAL = 100
         DEVICE_ID = 0
         ESC_KEY = 27
         WINDOW_NAME = 'facial recognition'
-        RED = (0, 0, 255)
-        BLUE = (255, 0, 0)
 
         cap = cv2.VideoCapture(DEVICE_ID)
         end_flag, frame = cap.read()
@@ -100,31 +92,55 @@ class Facial_Recogition():
                 break
 
             end_flag, c_frame = cap.read()
-
-            # Detect face and return bounding box (type: dlib.rectangle)
-            bounding_box = self.alignment.getLargestFaceBoundingBox(c_frame)
-            
-            if bounding_box != None:
-                cv2.rectangle(c_frame, (bounding_box.left(), bounding_box.top()), (bounding_box.right(), bounding_box.bottom()), BLUE, 2)
-                # Transform image using specified face landmark indices and crop image to 96x96
-                cropped_image = self.alignment.align(self.input_size, c_frame, bounding_box, landmarkIndices=AlignDlib.OUTER_EYES_AND_NOSE)
-                test_representation = self.model.predict(
-                    preprocess_opencv_image(cropped_image, self.input_size))[0, :]
-                name = self.find_cosine_distance(
-                    test_representation)
-                cv2.putText(
-                    c_frame, name, (bounding_box.left(), bounding_box.top()), cv2.FONT_HERSHEY_DUPLEX, SCALE, RED)
-
+            c_frame = self.predict(c_frame)
             cv2.imshow(WINDOW_NAME, c_frame)
 
         cv2.imwrite('last_frame.jpg', c_frame)
         cv2.destroyAllWindows()
         cap.release()
+    
+    def predict(self, c_frame):
+
+        RED = (0, 0, 255)
+        BLUE = (255, 0, 0)
+        SCALE = 1
+
+        # Detect face and return bounding box (type: dlib.rectangle)
+        bounding_box = self.alignment.getLargestFaceBoundingBox(c_frame)
+
+        if bounding_box != None:
+            cv2.rectangle(c_frame, (bounding_box.left(), bounding_box.top(
+            )), (bounding_box.right(), bounding_box.bottom()), BLUE, 2)
+            # Transform image using specified face landmark indices and crop image to 96x96
+            cropped_image = self.alignment.align(
+                self.input_size, c_frame, bounding_box, landmarkIndices=AlignDlib.OUTER_EYES_AND_NOSE)
+            test_representation = self.model.predict(
+                self.preprocess(cropped_image))[0, :]
+            name = self.find_distance(
+                test_representation)
+            name = self.find_distance(
+                test_representation, alg='euclidean')
+            cv2.putText(
+                c_frame, name, (bounding_box.left(), bounding_box.top()), cv2.FONT_HERSHEY_DUPLEX, SCALE, RED)
+        
+        return c_frame
+
+    def preprocess(self, image):
+        if type(image) is str:
+            img = load_img(image, target_size=(
+                self.input_size, self.input_size))
+            img = img_to_array(img)
+        else:
+            img = resize(image, (self.input_size, self.input_size))
+        img = np.expand_dims(img, axis=0)
+        img = preprocess_input(img) if self.architecture == 'VGG16' else img
+        return img
+
 
 if __name__ == '__main__':
 
     test_image_path = 'test.jpg'
-    fr = Facial_Recogition()
-    # fr.recognize_from_path(test_image_path)
+    fr = Facial_Recogition(architecture='Inception')
+    # fr.recognize(test_image_path)
     fr.recognize_realtime()
-    # fr.recognize_capture()
+    
