ce2d032ee0f3b457a7611a9146a759f2576ec596
Kei <35276788+keiohtani@users.noreply.github.com>
2019-04-23 00:02:30 -0400

    increase the accuracy for still images upto 80s

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.vgg16 import preprocess_input
 from cv2 import resize
 from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+import visulization
 
 
 class Facial_Recogition():
@@ -16,6 +18,7 @@ class Facial_Recogition():
     def __init__(self, architecture='VGG16', alg='svc'):
         self.architecture = architecture
         self.alg = alg
+        self.photo_id = 0
         if architecture == 'Inception':
             self.model = Inception_Model()
             self.input_size = 96
@@ -24,19 +27,21 @@ class Facial_Recogition():
             self.input_size = 224
         else:
             print('A valid name for model needs to be passed.')
-        if alg=='svc':
-            self.svc = SVC()
+        if alg == 'svc':
+            self.skmodel = SVC(kernel='poly', degree=3)
             dir_path = 'svc_face_database'
-            self.fit(dir_path)
-        else: 
+            X, y = self.load_data(dir_path)
+            self.skmodel.fit(X, y)
+        elif alg == 'knn':
+            self.skmodel = KNeighborsClassifier(n_neighbors=5)
+            dir_path = 'svc_face_database'
+            X, y = self.load_data(dir_path)
+            self.skmodel.fit(X, y)
+        else:
             dir_path = 'face_database'
+            self.image_dir_list = os.listdir(dir_path)
             self.np_dataset = self.load_database(dir_path)
 
-        # self.image_dir_list = os.listdir(dir_path)
-        # if ('.DS_Store' in self.image_dir_list):
-        #     self.image_dir_list.remove('.DS_Store')
-        #     print('.DS_Store is removed.')
-        
         print('Database is loaded.')
         self.alignment = AlignDlib('models/landmarks.dat')
 
@@ -44,9 +49,12 @@ class Facial_Recogition():
 
         dataset = []
         image_dir_list = os.listdir(dir_path)
+        if ('.DS_Store' in image_dir_list):
+            image_dir_list.remove('.DS_Store')
+            print('.DS_Store is removed.')
         for stored_image in image_dir_list:
             img_representation = self.model.predict(
-                self.preprocess(os.path.path.join(dir_path, stored_image)))[0, :]
+                self.preprocess(os.path.join(dir_path, stored_image)))[0, :]
             dataset.append(img_representation)
 
         return np.array(dataset)
@@ -98,17 +106,17 @@ class Facial_Recogition():
                 break
 
             end_flag, c_frame = cap.read()
-            if self.alg == 'svc':
-                c_frame = self.predict_svc(c_frame)
-            else: 
+            if self.alg == 'svc' or 'knn':
                 c_frame = self.predict(c_frame)
+            else:
+                c_frame = self.predict_by_distance(c_frame)
             cv2.imshow(WINDOW_NAME, c_frame)
 
         cv2.imwrite('last_frame.jpg', c_frame)
         cv2.destroyAllWindows()
         cap.release()
 
-    def predict(self, c_frame):
+    def predict_by_distance(self, c_frame):
 
         RED = (0, 0, 255)
         BLUE = (255, 0, 0)
@@ -131,8 +139,8 @@ class Facial_Recogition():
 
         return c_frame
 
-    def predict_svc(self, c_frame):
-    
+    def predict(self, c_frame):
+
         RED = (0, 0, 255)
         BLUE = (255, 0, 0)
         SCALE = 1
@@ -141,35 +149,46 @@ class Facial_Recogition():
         bounding_box = self.alignment.getLargestFaceBoundingBox(c_frame)
 
         if bounding_box != None:
-            cv2.rectangle(c_frame, (bounding_box.left(), bounding_box.top(
-            )), (bounding_box.right(), bounding_box.bottom()), BLUE, 2)
             # Transform image using specified face landmark indices and crop image to 96x96
             cropped_image = self.alignment.align(
                 self.input_size, c_frame, bounding_box, landmarkIndices=AlignDlib.OUTER_EYES_AND_NOSE)
             test_representation = self.model.predict(
                 self.preprocess(cropped_image))[0, :]
+            # path = os.path.join('realtime_images', str(self.photo_id) + '.jpg')
+            # cv2.imwrite(path, cropped_image)
+            # self.photo_id = self.photo_id + 1
+            cv2.rectangle(c_frame, (bounding_box.left(), bounding_box.top(
+            )), (bounding_box.right(), bounding_box.bottom()), BLUE, 2)
             # name = self.find_distance(test_representation)
-            name = self.svc.predict([test_representation])
-            print(name)
+            name = self.skmodel.predict([test_representation])
+            print(name[0])
+
             cv2.putText(
                 c_frame, name[0], (bounding_box.left(), bounding_box.top()), cv2.FONT_HERSHEY_DUPLEX, SCALE, RED)
 
         return c_frame
 
-    def fit(self, dir_path):
+    def load_data(self, dir_path):
 
         name_folders_dir = os.listdir(dir_path)
         X = []
         y = []
 
+        if ('.DS_Store' in name_folders_dir):
+            name_folders_dir.remove('.DS_Store')
+            print('.DS_Store is removed.')
+
         for name_folder_dir in name_folders_dir:
             name_list = os.listdir(os.path.join(dir_path, name_folder_dir))
+            if ('.DS_Store' in name_list):
+                name_list.remove('.DS_Store')
+            print('.DS_Store is removed.')
             for stored_image in name_list:
                 img_representation = self.model.predict(
                     self.preprocess(os.path.join(dir_path, name_folder_dir, stored_image)))[0, :]
                 X.append(img_representation)
-            y.append(name_folder_dir)
-        self.svc.fit(X, y)
+                y.append(name_folder_dir)
+        return X, y
 
     def preprocess(self, image):
         if type(image) is str:
@@ -182,6 +201,11 @@ class Facial_Recogition():
         # img = preprocess_input(img) if self.architecture == 'VGG16' else img
         return img
 
+    def test_svc(self, path):
+        X, y = self.load_data(path)
+        score = self.skmodel.score(X, y)
+        print(score)
+
 
 if __name__ == '__main__':
 
@@ -189,4 +213,4 @@ if __name__ == '__main__':
     fr = Facial_Recogition(architecture='VGG16', alg='svc')
     # fr.recognize_from_path(test_image_path)
     fr.recognize()
-
+    # fr.test_svc('test_dataset')
